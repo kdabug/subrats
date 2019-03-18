7c091d71dd32583e2f4eb4d980fefc563c2a54ba
Mdellit110 <46010937+Mdellit110@users.noreply.github.com>
2019-03-18 09:19:07 -0400

    Matt models (#5)
    * create model.js with user, stations and comments
    
    * fix minor errors in models.js
    
    * minor adjustment to email field

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import StationPage from "./components/StationList";
 import CommentForm from "./components/CommentForm";
 import UserProfile from "./components/UserProfile";
 import Contact from "./components/Contact";
-
+import Footer from "./components/Footer";
 class App extends Component {
   constructor(props) {
     super(props);
---
+++ models.js
@@ -0,0 +1,97 @@
+const Sequelize = require('Sequelize');
+
+const sequelize = new Sequelize({
+  database: 'subrats_db',
+  dialect: 'postgres',
+  operatorAliases: false,
+  define: {
+    underscored: true,
+  },
+});
+
+const User = sequelize.define( 'user', {
+  first_name: Sequelize.STRING,
+  last_name: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    unique: true, // checks if email is already being used
+    allowNull: false,
+    validate: {
+      isEmail: { // checks if input is in email form
+        msg: 'please enter a valid email address'
+      },
+      notNull: { // message given if null
+        msg: 'please enter an email address'
+      }
+    }
+  },
+  username: {
+    type: Sequelize.STRING,
+    unique: true, // checks if username is already being used
+    allowNull: false,
+    validate: {
+      notNull: { // message given if null
+        msg: 'please enter a username'
+      }
+    }
+  },
+  password_digest: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notNull: { // message given if null
+        msg: 'please enter a password'
+      }
+    }
+  },
+  is_local: Sequelize.BOOLEAN,
+});
+
+const Station = sequelize.define( 'Station', {
+  name: Sequelize.STRING,
+  latitude: Sequelize.INTEGER, // log and lat are for geolocation
+  longitude: Sequelize.INTEGER,
+});
+
+const Comment = sequelize.define( 'comments', {
+  activity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      len: [1,5], // allows only numbers 1-5
+    }
+  },
+  cleanliness: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      len: [1,5], // allows only numbers 1-5
+    }
+  },
+  wait_time: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      len: [1,5], // allows only numbers 1-5
+    }
+  },
+  opt_comment: Sequelize.STRING,
+  is_there: Sequelize.BOOLEAN,
+})
+
+// one to many
+Comment.belongsTo(User);
+Comment.belongsTo(Station);
+User.hasMany(Comment);
+Station.hasMany(Comment);
+
+// many to many
+User.belongsToMany(Station, { through: 'favorite_station' });
+Station.belongsToMany(User, { through: 'favorite_station' });
+
+module.exports = {
+  User,
+  Station,
+  Comment,
+  sequelize
+}
