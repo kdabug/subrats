1900adef773976debb9b717cce95f8a2dc70a244
matthew dellitalia <matthewdellitalia@ool-ad020328.dyn.optonline.net>
2019-03-21 13:35:40 -0400

    merge

--- client/src/services/users-helpers.js
+++ client/src/services/users-helpers.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = "https://tranquil-ravine-67605.herokuapp.com";
 
 const api = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: "https://tranquil-ravine-67605.herokuapp.com",
   headers: {
     authorization: `Bearer ${localStorage.getItem("jwt")}`
   }
